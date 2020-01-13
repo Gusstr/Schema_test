4f5cad9b7aad592d9abd579d645a33558ede47c0
Gustaf <gustaf.stralin@skola.botkyrka.se>
2020-01-13 09:33:59 +0100

    schema fungerar

--- quinyx.py
+++ quinyx.py
@@ -8,8 +8,13 @@ mydb = mysql.connector.connect(
 )
 
 mycursor = mydb.cursor()
+#sql = "DELETE FROM admin_account WHERE password = 'xam'"
+#mycursor.execute(sql)
+mydb.commit()
 
-mycursor.execute("SELECT * FROM user_account")
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM admin_account")
 
 myresult = mycursor.fetchall()
 
@@ -17,10 +22,36 @@ for x in myresult:
   print(x)
 
 
-Do = input("ny användare /nyanv, login /login: ")
+Do = input("ny användare /nyanv, login /login, ny admin /nyadmin: ")
 
 if Do == "/nyanv":
-    newfirstname = input("Förnamnamn: ")
-    newlastname = input("Efternamn: ")
-    newpassword = input("nytt password: ")
-    quinyx_Functions.new_user(newfirstname, newlastname, newpassword)
+  newfirstname = input("Förnamnamn: ")
+  newlastname = input("Efternamn: ")
+  newpassword = input("nytt password: ")
+  quinyx_Functions.new_user(newfirstname, newlastname, newpassword)
+elif Do == "/nyadmin":
+  newadminfirstname = input("Förnamnamn: ")
+  newadminlastname = input("Efternamn: ")
+  newadminpassword = input("nytt password: ")
+  newcompanyname = input("Företagsnamn: ")
+  quinyx_Functions.new_admin(newadminfirstname, newadminlastname, newadminpassword, newcompanyname)
+elif Do == "/loginadmin":
+  adminCname = input("Företagsnamn: ")
+  adminpassword = input("lösenord: ")
+  Result = quinyx_Functions.loginadmin(adminCname, adminpassword)
+  if Result == 0:
+    print("fel lösenord")
+  elif Result == 1:
+    print ("inloggad")
+    print(quinyx_Functions.show_schema(adminCname))
+    Do1 = input("lägg till /till: ")
+    if Do1 == "/till":
+      Ldag = input("dag: ")
+      Ltid = input("tid: ")
+      Ljobb = input("vad ska göras: ")
+      quinyx_Functions.insert_schema(Ldag, Ltid, Ljobb)
+    
+
+
+  
+  
--- quinyx_Functions.py
+++ quinyx_Functions.py
@@ -20,15 +20,59 @@ def new_user(A, B, C):
     mydb.commit()
 
 def new_admin(A, B, C, D):
-    insertname = B + "_" + C
+    insertname = A + "_" + B
     mycursor = mydb.cursor()
-    sql = "INSERT INTO admin_account (companyname, name, password) VALUES (%s, %s)"
-    val = (A, insertname, C)
+    sql = "INSERT INTO admin_account (companyname, name, password) VALUES (%s, %s, %s)"
+    val = (D, insertname, C)
     mycursor.execute(sql, val)
     mydb.commit()
 
-    companyscheam = A + "_schema"
-    mycursor.execute("CREATE TABLE 'companyschema' (time VARCHAR(255), date VARCHAR(255), work VARCHAR(255)")
+    companyscheam = D + "_schema"
+    sql = "CREATE TABLE {} (id INT AUTO_INCREMENT PRIMARY KEY, workingplace VARCHAR(255) DEFAULT '{}', time VARCHAR(255), date VARCHAR(255), work VARCHAR(255))".format(companyscheam, D)
+    mycursor.execute(sql)
+    mydb.commit()
+
+def loginadmin(A, B):
+  mycursor = mydb.cursor()
+  sql = "SELECT password FROM admin_account WHERE companyname = %s"
+  Cname = (A, )
+  mycursor.execute(sql, Cname)
+
+
+  myresult = mycursor.fetchall()
+  
+  clean_result = ""
+  for x in myresult:
+    password = clean_result + x[0]
+  
+  if password == B:
+    I = 1
+  else:
+    I = 0
+  mydb.commit()    
+  return(I)
+
+def show_schema(A):
+  global nameC
+  nameC = A + "_schema"
+  mycursor = mydb.cursor(buffered=True)
+  sql = "SELECT * FROM {}".format(nameC)
+  mycursor.execute(sql)
+  mydb.commit() 
+  myresult = mycursor.fetchall()   #inte klart
+  for x in myresult:
+    return x
+
+def insert_schema(A, B, C):
+  mycursor = mydb.cursor()
+  sql = "INSERT INTO {} (date, time, work) VALUES (%s, %s, %s)".format(nameC)
+  val = (A, B, C)
+  mycursor.execute(sql, val)
+  mydb.commit()  
+  
+
+
+
 
 
 
